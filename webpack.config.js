0d505b20ead6c03ef0815c4d8fde6c73f94dffa3
rudzkimichal <michal.rudzki@yahoo.com>
2020-04-20 15:19:26 +0200

    environment configured

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/components/App.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    publicPath: '/dist/',
+    compress: true,
+    port: 8080,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
