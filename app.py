72a2fb83b08536bfb35c7d58f8a8acb54252b92d
rudzkimichal <michal.rudzki@yahoo.com>
2021-02-25 22:39:53 +0100

    endpoint / set

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask, generate_template
+from flask import Flask, render_template
+import pymongo
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='client')
 
 @app.route('/')
 def index():
-    return generate_template('index.html')
+    client = pymongo.MongoClient('mongodb+srv://admin:6YMxZYttw6RaTK1t@books.eicd4.mongodb.net/books?retryWrites=true&w=majority')
+    db = client.books
+    db = db.test
+    data = db.find()
+
+    for item in data:
+        print(item)
+        return render_template('index.html', message = item)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
