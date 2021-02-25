971b063ffc7e6aae4ab9d771d77c56fa5f959ef7
rudzkimichal <michal.rudzki@yahoo.com>
2021-02-25 23:10:17 +0100

    api key moved to .env

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from flask import Flask, render_template
 import pymongo
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 app = Flask(__name__, template_folder='client')
 
 @app.route('/')
 def index():
-    client = pymongo.MongoClient('mongodb+srv://admin:6YMxZYttw6RaTK1t@books.eicd4.mongodb.net/books?retryWrites=true&w=majority')
+    api_key = os.environ.get('MONGO_URL')
+    client = pymongo.MongoClient(api_key)
     db = client.books
     db = db.test
     data = db.find()
-    print(os.environ)
+    print(os.environ['MONGO_URL'])
 
     for item in data:
-        # print(item)
+        print(item)
         return render_template('index.html', message = item)
 
 if __name__ == '__main__':
